c97a4f7bddb8f7b77cf1e5211b4abb71bbb066eb
Lyxnx <a.julnes@gmail.com>
2020-07-07 22:55:30 +0100

    Migrate to rx observables

--- app/src/main/java/net/lyxnx/reginfo/tasks/History.java
+++ app/src/main/java/net/lyxnx/reginfo/tasks/History.java
@@ -1,9 +1,15 @@
 package net.lyxnx.reginfo.tasks;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import lombok.Data;
+import net.lyxnx.reginfo.BuildConfig;
 
 import java.io.*;
 import java.time.LocalDateTime;
@@ -13,6 +19,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class History {
+    private static final String TAG = "HISTORY";
+    
+    //////////////////////////////////////
+    
+    private History() {}
+    
     private static volatile History instance;
     
     public static History getInstance() {
@@ -27,41 +39,59 @@ public class History {
         return instance;
     }
     
+    //////////////////////////////////////
+    
+    private static final String HISTORY_FILE = "history";
+    
     private List<HistoryItem> items;
     
-    private History() {}
+    private boolean initialised = false;
     
-    private boolean inited = false;
-    public void init(Context context) {
-        // only want to do initialisation once
-        if (inited)
+    public void initialise(Context context) {
+        // We only want to do initialisation once
+        if (initialised) {
             return;
+        }
         
-        File historyFile = new File(context.getFilesDir(), "history");
+        readItems(context)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        historyItems -> History.this.items = historyItems,
+                        throwable -> {
+                            if (BuildConfig.DEBUG) {
+                                Log.e(TAG, "Could not read data from history file", throwable);
+                            }
+                        }
+                );
+        
+        initialised = true;
+    }
+    
+    private Flowable<List<HistoryItem>> readItems(Context context) {
+        File historyFile = new File(context.getFilesDir(), HISTORY_FILE);
         
         if (!historyFile.exists()) {
             try {
                 historyFile.createNewFile();
             } catch (IOException e) {
-                e.printStackTrace();
+                return Flowable.error(e);
             }
         }
+        
+        List<HistoryItem> historyItems;
         
         Gson gson = new Gson();
         
         try (BufferedReader reader = new BufferedReader(new FileReader(historyFile))) {
             List<HistoryItem> list = gson.fromJson(reader, new TypeToken<List<HistoryItem>>(){}.getType());
             
-            if (list == null) {
-                this.items = new ArrayList<>();
-            } else {
-                this.items = list;
-            }
+            historyItems = list == null ? new ArrayList<>() : list;
         } catch (IOException e) {
-            e.printStackTrace();
+            return Flowable.error(e);
         }
-    
-        inited = true;
+        
+        return Flowable.just(historyItems);
     }
     
     public boolean isEmpty() {
@@ -84,11 +114,27 @@ public class History {
     }
     
     public void save(Context context) {
+        save0(context)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        v -> {},
+                        t -> {
+                            if (BuildConfig.DEBUG) {
+                                Log.e(TAG, "Could not read data from history file", t);
+                            }
+                            
+                            Toast.makeText(context, "Could not save history to file: " + t.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                );
+    }
+    
+    private Flowable<Void> save0(Context context) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(context.getFilesDir(), "history")))) {
             new Gson().toJson(this.items, writer);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            Toast.makeText(context, "Could not save history to file: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+            return Flowable.empty();
+        } catch (IOException e) {
+            return Flowable.error(e);
         }
     }
     
@@ -96,27 +142,15 @@ public class History {
      * Returns a copy of the list, don't use any update methods from this
      * This will reverse it for display
      */
-    public List<HistoryItem> getItems() {
+    public List<HistoryItem> getOrderedItems() {
         List<HistoryItem> list = new ArrayList<>(items);
         Collections.reverse(list);
         return list;
     }
     
+    @Data
     public static class HistoryItem {
         private final String vrm;
         private final String date;
-    
-        HistoryItem(String vrm, String date) {
-            this.vrm = vrm;
-            this.date = date;
-        }
-    
-        public String getVrm() {
-            return vrm;
-        }
-    
-        public String getDate() {
-            return date;
-        }
     }
 }
